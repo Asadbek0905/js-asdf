624c1d8d09a0378ea391c1348d1f1a79d3b7496f
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-03-27 13:26:43 +0500

    fix

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-// let ab = +prompt("Sovg'ani narxini kiriting")
+let ab = +prompt("Sovg'ani narxini kiriting")
 
-// function personalPrice(a, b, s, d) {
-//     let jami = a + b + s + d
+function personalPrice(a, b, s, d) {
+    let jami = a + b + s + d
 
-//     if(jami <= ab){
-//         alert("Ha pulingiz yetadi");
-//     }else{
-//         alert("Yoq pulingiz yetmaydi");
-//     }
+    if(jami <= ab){
+        alert("Ha pulingiz yetadi");
+    }else{
+        alert("Yoq pulingiz yetmaydi");
+    }
+}
+
+console.log(personalPrice(20, 10, 30, 20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let inp = document.querySelector("input")
+// let btn = document.querySelector(".b1")
+// let text = document.querySelector("h1")
+// let btn1 = document.querySelector(".b2")
+
+// btn.onclick = () => {
+//     text.innerHTML = `
+//     <h1 class="to">${inp.value}</h1>
+//     `
+//     inp.value = ""
 // }
 
-// console.log(personalPrice(20, 10, 30, 20));
-
-
-
-
-
-
-
-
-let inp = document.querySelector("input")
-let btn = document.querySelector(".b1")
-let text = document.querySelector("h1")
-let btn1 = document.querySelector(".b2")
-
-btn.onclick = () => {
-    text.innerHTML = `
-    <h1 class="to">${inp.value}</h1>
-    `
-    inp.value = ""
-}
-
-btn1.onclick = () => {
-    text.innerHTML = `
-    <h1 class="to">ToDo List</h1>
-    `
-}
+// btn1.onclick = () => {
+//     text.innerHTML = `
+//     <h1 class="to">ToDo List</h1>
+//     `
+// }
