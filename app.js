c8213409b26b4ff450d53ae49bc5420e32eb189c
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-04-03 16:22:31 +0500

    fix

--- app.js
+++ app.js
@@ -6,11 +6,11 @@
 //--------------------------------------(4)
 
 
-let myFunc = (a) => {
-    return a.reverse(a)
-}
+// let myFunc = (a) => {
+//     return a.reverse(a)
+// }
 
-console.log(myFunc([1, 2, 3, 4, 5]));
+// console.log(myFunc([1, 2, 3, 4, 5]));
 
 
 
@@ -23,31 +23,39 @@ console.log(myFunc([1, 2, 3, 4, 5]));
 //----------------------------------------------------------(1)
 
 
-let name = (a, b, c) => {
-    let funcm = (a, b, c) => {
-        if (a >= b && a >= c) {
-            return a
-        } else if (b >= a && b >= c) {
-            return b
-        } else if (c >= a && c >= b) {
-            return c
+let w = document.querySelector(".w")
+w.onclick = () => {
+    let a = +prompt("1-sonni kiriting")
+    let b = +prompt("2-sonni kiriting")
+    let c = +prompt("3-sonni kiriting")
+    
+    let name = (a, b, c) => {
+        let funcm = (a, b, c) => {
+            if (a >= b && a >= c) {
+                return a
+            } else if (b >= a && b >= c) {
+                return b
+            } else if (c >= a && c >= b) {
+                return c
+            }
         }
-    }
-
-    let func = () => {
-        if (a <= b && a >= c) {
-            return a
-        } else if (b <= a && b <= c) {
-            return b
-        } else if (c >= a && c >= b) {
-            return c
+    
+        let func = () => {
+            if (a <= b && a >= c) {
+                return a
+            } else if (b <= a && b <= c) {
+                return b
+            } else if (c >= a && c >= b) {
+                return c
+            }
         }
+    
+        return `Max: ${funcm(a, b, c)}, Min: ${func()}`
     }
-
-    return `Max: ${funcm(a, b, c)}, Min: ${func()}`
+    
+    alert(name(a, b, c))
 }
 
-console.log(name(50, 30, 70));
 
 
 
@@ -65,10 +73,14 @@ console.log(name(50, 30, 70));
 
 //------------------------------------------------(2)
 
-function Myfunc(a, b) {
-    return Math.floor(b / a)
-}
-console.log(Myfunc(5, 15));
+
+// let a = +prompt("niqob sonini kiriting")
+// let b = +prompt("odam Sonni kiriting")
+
+// function Myfunc(a, b) {
+//     return Math.floor(b / a)
+// }
+// console.log(Myfunc(a, b));
 
 
 
@@ -79,9 +91,9 @@ console.log(Myfunc(5, 15));
 //---------------------------------------------(3)
 
 
+// let a = +prompt("son kiritng")
+// let MyFunc = (a) => {
+//     return Math.floor(a / 10)
+// }
 
-let MyFunc = (a) => {
-    return Math.floor(a / 10)
-}
-
-console.log(MyFunc(60));
+// console.log(MyFunc(a));
